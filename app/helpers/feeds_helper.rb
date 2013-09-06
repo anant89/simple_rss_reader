79f15b8bb79edbc0859a1e40c236711c9c3acaac
anant89 <ani.kolvankar@gmail.com>
2013-09-06 11:03:23 +0530

    heading change for particular rss feed

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -6,6 +6,9 @@ end
 
 def getting_feed feeditem
            rss = RSS::Parser.parse(open(feeditem.feed_url).read, false)
+              if params[:action] == 'feed'
+               @title = rss.channel.title
+              end
 	      rss.items.take(10).each do |i|
                @gradesm[i.date]= [i.title,i.link,i.description]
 	      end
