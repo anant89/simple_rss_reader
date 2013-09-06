c4d42f4c68d836ae5a321fce37c6e9c66f5bf6ef
anant89 <ani.kolvankar@gmail.com>
2013-09-06 10:43:18 +0530

    helper method used in controller

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -1,2 +1,13 @@
 module FeedsHelper
+def sort_key gradesm
+@grades_keys = gradesm.keys.sort.reverse.paginate(:page => params[:page],:per_page => 5)
+
+end
+
+def getting_feed feeditem
+           rss = RSS::Parser.parse(open(feeditem.feed_url).read, false)
+	      rss.items.take(10).each do |i|
+               @gradesm[i.date]= [i.title,i.link,i.description]
+	      end
+end
 end
